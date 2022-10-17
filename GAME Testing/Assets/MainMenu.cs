80aeab0ef44167a7d4ed12c4169ee2d1378ce76e
Alfian Dorif Murtadlo <100700571+IndyJones123@users.noreply.github.com>
2022-10-17 07:01:34 +0700

    Penambahan Setting UI & Linking

--- GAME Testing/Assets/MainMenu.cs
+++ GAME Testing/Assets/MainMenu.cs
@@ -8,4 +8,8 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame(){
+        Debug.Log("Quit");
+        Application.Quit();
+    }
 }
