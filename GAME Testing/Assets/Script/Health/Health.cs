32f6580ccfe04d0048c16db90ae3196e9b6a68dc
Alfian Dorif Murtadlo <100700571+IndyJones123@users.noreply.github.com>
2022-10-24 21:18:49 +0700

    Menambahkan Kobo Terluka & Mati

--- GAME Testing/Assets/Script/Health/Health.cs
+++ GAME Testing/Assets/Script/Health/Health.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+    }
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            //iframes
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
 }
