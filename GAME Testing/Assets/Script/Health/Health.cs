b0dcd0094e9d33b15ebe088e5bbdf55cf06b6e61
yoya4 <yaholila4@gmail.com>
2022-10-25 11:05:53 +0700

    iFrame selesai

--- GAME Testing/Assets/Script/Health/Health.cs
+++ GAME Testing/Assets/Script/Health/Health.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using System.Collections;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
@@ -9,10 +11,16 @@ public class Health : MonoBehaviour
     [SerializeField] private AudioClip Uek;
     [SerializeField] private AudioClip mati;
 
+    [Header ("iFrames")]
+    [SerializeField]private float iFramesDuration;
+    [SerializeField]private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -22,7 +30,7 @@ public class Health : MonoBehaviour
         {
             SoundManagerScript.instance.PlaySound(Uek);
             anim.SetTrigger("hurt");
-            //iframes
+            StartCoroutine(Invunerability());
         }
         else
         {
@@ -39,4 +47,17 @@ public class Health : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
+
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        for(int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+    } 
 }
