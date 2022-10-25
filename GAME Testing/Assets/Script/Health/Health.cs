127650ae8b90998937ecc668c14dfd1a71583b0a
Alfian Dorif Murtadlo <100700571+IndyJones123@users.noreply.github.com>
2022-10-25 09:55:37 +0700

    Revert "Menambahkan Kobo Terluka & Mati"
    This reverts commit 32f6580ccfe04d0048c16db90ae3196e9b6a68dc.

--- GAME Testing/Assets/Script/Health/Health.cs
+++ GAME Testing/Assets/Script/Health/Health.cs
@@ -1,38 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private float startingHealth;
-    public float currentHealth { get; private set; }
-    private Animator anim;
-    private bool dead;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        currentHealth = startingHealth;
-        anim = GetComponent<Animator>();
+        
     }
-    public void TakeDamage(float _damage)
-    {
-        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
-        if (currentHealth > 0)
-        {
-            anim.SetTrigger("hurt");
-            //iframes
-        }
-        else
-        {
-            if (!dead)
-            {
-                anim.SetTrigger("die");
-                GetComponent<PlayerMovement>().enabled = false;
-                dead = true;
-            }
-        }
-    }
-    public void AddHealth(float _value)
+    // Update is called once per frame
+    void Update()
     {
-        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+        
     }
 }
