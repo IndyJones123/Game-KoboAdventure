0afad724ededda6e0b1efcfcc2e1c05b00cddd11
Alfian Dorif Murtadlo <100700571+IndyJones123@users.noreply.github.com>
2022-11-02 07:45:33 +0700

    Fixed Fitur Serangan Range Main Character + Trap

--- GAME Testing/Assets/Script/Player/PlayerAttack.cs
+++ GAME Testing/Assets/Script/Player/PlayerAttack.cs
@@ -5,6 +5,7 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private float attackCooldown;
     [SerializeField] private Transform FirePoint;
     [SerializeField] private GameObject[] Fireballs;
+    [SerializeField] private AudioClip Ehe;
 
     private Animator anim;
     private PlayerMovement playerMovement;
@@ -27,6 +28,7 @@ public class PlayerAttack : MonoBehaviour
     private void Attack()
     {
         anim.SetTrigger("attack");
+        SoundManagerScript.instance.PlaySound(Ehe);
         cooldownTimer = 0;
 
         Fireballs[FindFireball()].transform.position = FirePoint.position;
--- GAME Testing/Assets/Script/Rooms/SoundManagerScript.cs
+++ GAME Testing/Assets/Script/Rooms/SoundManagerScript.cs
@@ -22,5 +22,5 @@ public class SoundManagerScript : MonoBehaviour
     {
         source.PlayOneShot(_sound);
     }
-
+    
 }
