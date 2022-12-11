79ede154f13d1ae4af60d0fd53146f29613e79bc
Alfian Dorif Murtadlo <100700571+IndyJones123@users.noreply.github.com>
2022-12-11 07:47:04 +0700

    Sound

--- GAME Testing/Assets/SpikeDamage.cs
+++ GAME Testing/Assets/SpikeDamage.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class SpikeDamage : MonoBehaviour
 {
 [SerializeField] public int damage;
+[SerializeField] private AudioClip GSH;
+public SpriteRenderer spriteRenderer;
+
+     void Start()
+ {
+     this.spriteRenderer = GetComponent<SpriteRenderer>();
+ }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
+        {
             Debug.Log("test");
             collision.GetComponent<HealthPlayer>().PlayerTakeDamage(damage);
+            
+        }
+            
+            
+
+        if(collision.tag == "Ckia")
+        {
+            this.spriteRenderer.enabled = true;
+            SoundManagerScript.instance.PlaySound(GSH);
+        }
     }
 }
